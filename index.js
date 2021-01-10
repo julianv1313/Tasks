36a9498d9d6b79a0320480575225b31b4d1a961f
Julian <jv.julian@hotmail.com>
2021-01-09 19:34:32 -0500

    login y logout

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Note = require("./models/Note")
-const cookieSession = require("cookie-session")
+const Note = require("./models/Note");
+const User = require("./models/User");
+const cookieSession = require("cookie-session");
 const md = require("marked");
+
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/Notes", { useNewUrlParser: true});
-
-
-
-/*const logger = (req, res, next) => {
-    console.log("Nueva petición http");
-    next();
-};*/
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -22,21 +17,23 @@ app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000
 }));
 app.use("/assets", express.static("assets"));
-/*app.use(logger);*/
 
-// muestra la lista de notas
+// Muestra la lista de notas.
+
 app.get('/', async (req, res) => {
    const notes = await Note.find();
     res.render("index", { notes });
 });
 
-// muestra el formulario para crear una nota
+// Muestra el formulario para crear una nota.
+
 app.get("/notes/new", async (req, res) => {
     const notes = await Note.find();
     res.render("new", { notes });
 });
 
-// permite crear una nota
+// Permite crear una nota.
+
 app.post("/notes", async (req, res, next) => {
     const data = {
         title: req.body.title,
@@ -53,23 +50,7 @@ app.post("/notes", async (req, res, next) => {
     res.redirect("/");
 });
 
-/*app.get("/users/:name", (req, res) => {
-    const name = req.params.name;
-    res.send(`<h1>Hola ${name}</h1>`);
-});
-
-app.post("/users", (req, res) => {
-    res.status(404);
-    res.set("X-api-Version", "1.0");
-    res.send("no se encontró la ruta");
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Algo salio mal");
-});*/
-
-// muestra una nota
+// Muestra una nota.
 
 app.get("/notes/:id", async (req, res) => {
     const notes = await Note.find({ user: res.locals.user });
@@ -77,7 +58,7 @@ app.get("/notes/:id", async (req, res) => {
     res.render("show", { notes: notes, currentNote: note, md: md });
   });
 
-//muestra el formulario para editar
+// Muestra el formulario para editar.
 
 app.get("/notes/:id/edit", async (req, res, next) => {
     try {
@@ -92,7 +73,8 @@ app.get("/notes/:id/edit", async (req, res, next) => {
 });
 
 
-// Actualiza una nota  
+// Actualiza una nota.  
+
 app.patch("/notes/:id", async  (req, res, next) => {
     const id = req.params.id;
     const note = await Note.findById(id);
@@ -110,6 +92,8 @@ app.patch("/notes/:id", async  (req, res, next) => {
     
 });
 
+// Elimina una nota.
+
 app.delete("/notes/:id", async (req, res, next) => {
     try {
         await Note.deleteOne({ _id: req.params.id});
@@ -117,6 +101,47 @@ app.delete("/notes/:id", async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", async (req, res, next) => {
+    try {
+        const user = await User.create({
+            email: req.body.email,
+            password: req.body.password,
+        });
+        res.redirect("/login");
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", async (req, res, next) => {
+    try {
+        const user = await User.authenticate(req.body.email, req.body.password);
+        if (user) {
+            req.session.userId = user._id;
+            return res.redirect("/");
+        } else {
+            res.render("/login", { error: "wrong email or password. try again!"});
+        }
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.get("/logout", (req, res) => {
+    res.session = null;
+    res.clearCookie("session");
+    res,clearCookie("session.sig");
+    res.redirect("/login");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
